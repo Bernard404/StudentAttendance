057a8ace589c08a608f84033a724d16821504198
Bernard <bernard-steemers@hotmail.com>
2020-01-08 16:41:27 +0000

    Update studentTest.java

--- tests/studentTest.java
+++ tests/studentTest.java
@@ -2,9 +2,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by meudecc on 15/02/2017.
- */
 public class studentTest {
     @org.junit.Test(expected = IllegalArgumentException.class)
     public void belowZero() throws Exception {
